a6f6168f0b17cb593084fba1e2e9afb8bbe808c0
siddharth <siddharth.god@gmail.com>
2013-09-22 22:01:19 +0530

    Some more initial stuff

---
+++ src/in/co/sh00nya/cmn/ErrorCodes.java
@@ -0,0 +1,5 @@
+package in.co.sh00nya.cmn;
+
+public enum ErrorCodes {
+	
+}
---
+++ src/in/co/sh00nya/cmn/ServerException.java
@@ -0,0 +1,15 @@
+package in.co.sh00nya.cmn;
+
+public class ServerException extends Exception {
+	
+	private int errorCode;
+
+	public ServerException(int errorCode) {
+		super();
+	}
+
+	public int getErrorCode() {
+		return errorCode;
+	}
+
+}
